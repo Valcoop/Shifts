298bc18e744aa8e2f44a8023bbc02daf1ce51a2c
OlivierLardenois <olivier@gymlib.com>
2021-05-07 14:01:03 +0200

    🚧 - Handle user in AuthGuard

--- backend/src/auth/auth.guard.ts
+++ backend/src/auth/auth.guard.ts
@@ -2,25 +2,28 @@ import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { GqlExecutionContext } from '@nestjs/graphql';
 import parser from 'fast-xml-parser';
 import fetch from 'node-fetch';
+import { User } from '../users/users.entity';
 import { NextcloudUser } from './auth.controller';
+import { Request } from 'express';
+
+export interface AuthenticatedContext {
+  req: Request & { user?: User };
+}
 
 @Injectable()
 export class AuthGuard implements CanActivate {
   async canActivate(context: ExecutionContext): Promise<boolean> {
-    const ctx = GqlExecutionContext.create(context).getContext();
+    const { req } = GqlExecutionContext.create(
+      context,
+    ).getContext() as AuthenticatedContext;
 
     const accessToken =
-      ctx.req.header('Authorization') &&
-      (ctx.req.header('Authorization') as string).split('Bearer ')[1];
-
-    const userID = ctx.req.header('X-user-id');
-
-    // TODO: Vérifque le user existe dans notre base. Ne serait il pas mieux de find le user dans notre base autre part et de le mettre dans le context
-
-    if (!accessToken || !userID) return false;
+      req.header('Authorization') &&
+      (req.header('Authorization') as string).split('Bearer ')[1];
+    if (!accessToken || !req.user) return false;
 
     const data = await fetch(
-      'http://localhost:8080/ocs/v1.php/cloud/users/' + userID,
+      'http://localhost:8080/ocs/v1.php/cloud/users/' + req.user.id,
       { headers: { Authorization: 'Bearer ' + accessToken } },
     );
 
--- backend/src/login.middleware.ts
+++ backend/src/login.middleware.ts
@@ -1,13 +1,17 @@
 import { Injectable, NestMiddleware } from '@nestjs/common';
-import { NextFunction, Request, Response } from 'express';
-import { User } from './users/users.entity';
+import { NextFunction, Response } from 'express';
+import { AuthenticatedContext } from './auth/auth.guard';
 import { UsersService } from './users/users.service';
 
 @Injectable()
 export default class LoginMiddleware implements NestMiddleware {
   constructor(private usersService: UsersService) {}
 
-  async use(req: Request & { user?: User }, __: Response, next: NextFunction) {
+  async use(
+    req: AuthenticatedContext['req'],
+    __: Response,
+    next: NextFunction,
+  ) {
     const userID = req.header('X-user-id');
     const user = userID
       ? await this.usersService.findOne({ where: { externalID: userID } })
