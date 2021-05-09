ecbe9c877aec751434035afaa19d3968889c0f79
OlivierLardenois <olivier@gymlib.com>
2021-05-09 13:48:37 +0200

    🚧 - Use env var in oauth2 client setup

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { AuthorizationCode } from 'simple-oauth2';
 import { Repository } from 'typeorm';
+import { OAUTH_AUTHORIZE_PATH, OAUTH_TOKEN_PATH } from '../constants';
 import { User } from '../users/users.entity';
 import { UserDAO } from '../users/users.service';
 import { NextcloudUser } from './auth.controller';
@@ -13,19 +14,23 @@ export class AuthService {
   constructor(
     @InjectRepository(User) private userRepository: Repository<User>,
   ) {
+    if (
+      !process.env.OAUTH_HOST ||
+      !process.env.OAUTH_CLIENT_ID ||
+      !process.env.OAUTH_SECRET
+    ) {
+      throw new Error();
+    }
+
     this.client = new AuthorizationCode({
       client: {
-        // TODO: use env var
-        id: '8aSPndK3XtZsPbbfOugInfpi6FDZI1K6QsFBG6vbLKEZKdzfDbcubMQtL3R9Rh28',
-        // TODO: use env var
-        secret:
-          'nSnFd5kNIaJpcgvwe4HtiYRKijhl93SeNiPvYyNhpvytdoOZimLKTwXqQNuZqE9Q',
+        id: process.env.OAUTH_CLIENT_ID,
+        secret: process.env.OAUTH_SECRET,
       },
       auth: {
-        // TODO: use env var
-        tokenHost: 'http://localhost:8080',
-        tokenPath: '/apps/oauth2/api/v1/token',
-        authorizePath: '/apps/oauth2/authorize',
+        tokenHost: process.env.OAUTH_HOST,
+        tokenPath: OAUTH_TOKEN_PATH,
+        authorizePath: OAUTH_AUTHORIZE_PATH,
       },
       options: { authorizationMethod: 'body' },
     });
---
+++ backend/src/constants.ts
@@ -0,0 +1,2 @@
+export const OAUTH_TOKEN_PATH = '/apps/oauth2/api/v1/token';
+export const OAUTH_AUTHORIZE_PATH = '/apps/oauth2/authorize';
